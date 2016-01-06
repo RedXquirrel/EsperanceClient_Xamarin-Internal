6f7787b53f2f0ec0f72fc909f456bc89297b7211
Xamtastic <anthony.harrison@xamtastic.com>
2016-01-06 16:44:22 +0000

    Donky SDK Initialisation button in MainPageViewModel

--- EsperanceClient/ViewModels/MainPageViewModel.cs
+++ EsperanceClient/ViewModels/MainPageViewModel.cs
@@ -4,16 +4,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace EsperanceClient.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
+        public ICommand _initialiseDonkyCommand;
+        public ICommand InitialiseDonkyCommand
+        {
+            get
+            {
+                return _initialiseDonkyCommand;
+            }
+            set
+            {
+                _initialiseDonkyCommand = value;
+                RaisePropertyChanged("InitialiseDonkyCommand");
+            }
+        }
+
         private string _message = "Hello SmallestMvvm!";
         public string Message
         {
             get { return _message; }
             set { _message = value; RaisePropertyChanged("Message"); }
+        }
+
+        public MainPageViewModel()
+        {
+            InitialiseDonkyCommand = new Command<string>((key) =>
+            {
+                var a = 0;
+            });
+
         }
     }
 }
